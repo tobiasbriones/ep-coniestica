364ba0244017303de7b0548bd879340b37ba127a
Tobias Briones <tobiasbrionesdev@gmail.com>
2019-11-29 19:20:13 -0600

    services mjs file fixed

--- js/services.mjs
+++ js/services.mjs
@@ -20,53 +20,9 @@
  */
 
 import { NavigationManager } from './navigation.mjs';
-import { COMPANY_MISSION_CARD_HTML, COMPANY_VISION_CARD_HTML, COMPANY_OBJECTIVES_CARD_HTML } from './model.mjs';
 
 const navigationManager = new NavigationManager();
-var cardsParentEl;
-
-const getURLParam = paramName => {
-  const urlStr = window.location.href;
-  const url = new URL(urlStr);
-  return url.searchParams.get("v");
-}
-
-const gotoMission = () => {
-  history.pushState({}, 'Coniestica - Misión de la empresa', '/about.html?v=mission');
-  cardsParentEl.innerHTML = COMPANY_MISSION_CARD_HTML;
-}
-
-const gotoVision = () => {
-  history.pushState({}, 'Coniestica - Visión de la empresa', '/about.html?v=vision');
-  cardsParentEl.innerHTML = COMPANY_VISION_CARD_HTML;
-}
-
-const gotoObjectives = () => {
-  history.pushState({}, 'Coniestica - Objetivos de la empresa', '/about.html?v=objectives');
-  cardsParentEl.innerHTML = COMPANY_OBJECTIVES_CARD_HTML;
-}
 
 document.addEventListener('DOMContentLoaded', () => {
-  cardsParentEl = document.querySelector('#content > .cards');
-  const param = getURLParam('v');
-
   navigationManager.init();
-  document.getElementById('missionButton').addEventListener('click', gotoMission);
-  document.getElementById('visionButton').addEventListener('click', gotoVision);
-  document.getElementById('objectivesButton').addEventListener('click', gotoObjectives);
-
-  switch(param) {
-    case 'vision':
-      gotoVision();
-      break;
-
-    case 'objectives':
-      gotoObjectives();
-      break;
-
-    default:
-      gotoMission();
-      break;
-  }
-
 });
